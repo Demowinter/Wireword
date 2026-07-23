ba1bee0628dadac3d26d00cd6234db95d3835439
Nullnore <vitaliygr2301@gmail.com>
2026-07-23 15:44:07 +0300

    Add basic funtions and members to server class

--- include/server.hpp
+++ include/server.hpp
@@ -1,14 +1,33 @@
 #pragma once
+#include <string>
+#include <cstdint>
 #include <def.hpp>
 #include <logger.hpp>
+#include <eventloop.hpp>
+#include <handlereg.hpp>
 #include <socket.hpp>
 
 class Server {
 public:
     Server(Logger& logger);
 
-private:
-    ModuleLogger mlogger;
+    void openClientListener(const std::string& addr, uint16_t port);
+    void openModuleListener(const std::string& addr, uint16_t port);
 
-    SocketManager sockmgr;
+    void run();
+    void stop();
+
+    static void interruptHandler(int32_t interrupt);
+
+private:
+    Logger& logger;
+    ModuleLogger mlogger{"Server", logger};
+
+    EventLoop eventloop;
+    HandleRegistry hreg;
+
+    SocketManager clientmgr{logger, eventloop, hreg};
+    SocketManager modulemgr{logger, eventloop, hreg};
+
+    static inline Server* instance = nullptr;
 };
