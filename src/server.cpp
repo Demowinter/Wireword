5449048d4bdb91260a5dfa5c4c1dc9c1770b509b
Nullnore <vitaliygr2301@gmail.com>
2026-07-23 15:42:22 +0300

    Add server.cpp file

---
+++ src/server.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <csignal>
+#include <cstdint>
+#include <def.hpp>
+#include <logger.hpp>
+#include <server.hpp>
+
+Server::Server(Logger& logger) : logger{logger} { instance = this; }
+
+void Server::openClientListener(const std::string& addr, uint16_t port) {
+    clientmgr.open(addr, port);
+}
+
+void Server::openModuleListener(const std::string& addr, uint16_t port) {
+    modulemgr.open(addr, port);
+}
+
+void Server::run() {
+    mlogger.log("Server running!");
+
+    eventloop.run();
+}
+
+void Server::stop() {
+    mlogger.log("Server stopped!");
+
+    eventloop.stop();
+}
+
+void Server::interruptHandler(int32_t interrupt) {
+    if (interrupt == SIGINT) {
+        instance->mlogger.log("Received interrupt signal, stopping server...");
+
+        instance->stop();
+    }
+}
